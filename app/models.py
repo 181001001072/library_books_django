2b12d0efd878444b18e969d7eab6a88f051144c1
unknown <itcdea08@iiht.tech>
2022-09-19 13:06:27 +0530

    added requirements txt

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class Books(models.Model):
     book_name = models.CharField(max_length=255)
     book_author = models.CharField(max_length=255)
     issued_from = models.ForeignKey(Library,on_delete=models.CASCADE)
+    image = models.FileField(upload_to="blog/app/media/Books",null=True,blank=True)
 
     def __str__(self):
         return self.book_name
--- app/views.py
+++ app/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
-from .models import Library,Books
+from .models import Books
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
 
 
 def index(request):
-    books = Books.objects.all().values("issued_from__name","issued_from__id","issued_from__location","book_name","book_author","issued_from__id")
-    print(books)
+    books = Books.objects.all().values("issued_from__name","issued_from__id","issued_from__location","book_name","book_author","issued_from__id","image")
+    
     return render (request,"index.html",{"books":books})
 
 def search(request):
@@ -23,13 +25,23 @@ def signup(request):
     return render(request,"signup.html")
 
 def signup_action(request):
-    username = request.BOOKS.get("email")
-    password = request.BOOKS.get("password")
-    name = request.BOOKS.get("name")
+    username = request.POST.get("email")
+    password = request.POST.get("password")
+    name = request.POST.get("name")
     user = User.objects.create_user(username=username,password=password,first_name=name)
     auth.login(request,user=user)
     return redirect('index')
 
+def signin(request):
+    return render(request,"signin.html")
+
+def signin_action(request):
+    username = request.POST.get("email")
+    password = request.POST.get("password")
+    print(username,password)
+    user = auth.authenticate(request,username=username,password=password)
+    auth.login(request,user)
+    return redirect('index')
 
 def Payel(request):
     return HttpResponse('<h1>Hii</h1>')
